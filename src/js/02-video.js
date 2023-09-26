757d5110976c2e11442950506ebbe34688a69376
Ivan Kr <126171248+Dofff95@users.noreply.github.com>
2023-09-26 15:53:53 +0300

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+// import Player from '@vimeo/player';
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
 
+// const player = new Player('handstick', {
+//   id: 19231868,
+//   width: 640,
+// });
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
 
+const feedback = {
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+  form: document.querySelector('.feedback-form'),
+};
+const formData = {};
+
+feedback.form.addEventListener('input', throttle(inputData, 500));
+function inputData(evt) {
+  formData[evt.target.name] = evt.target.value.trim();
+  JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  console.log(formData);
+}
+
+feedback.form.addEventListener('submit', throttle(submitForm, 500));
+function submitForm(evt) {
+  evt.preventDefault();
+  const { email, message } = evt.currentTarget.elements;
+  console.log({ email: email.value.trim(), message: message.value.trim() });
+  localStorage.removeItem(STORAGE_KEY);
+  evt.currentTarget.reset();
+}
+writeFromStorage();
+function writeFromStorage() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const savedEmail = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+    feedback.textarea.value = savedMessage || '';
+    feedback.input.value = savedEmail || '';
+  }
+}
